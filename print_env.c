16ae825ee31f630f1d8137fa2bd9a851b15d3016
kiage <123022606+roviae@users.noreply.github.com>
2023-05-26 21:06:41 +0300

    deleted the pathfinder file and reviewed the environment file

--- print_env.c
+++ print_env.c
@@ -5,7 +5,7 @@
  *
  * Return: void
  */
-int print_env(void)
+void print_env(void)
 {
 	char **env = environ;
 	int i = 0;
@@ -15,68 +15,5 @@ int print_env(void)
 		printf("%s\n", env[i]);
 		i++;
 	}
-	return (0);
 
-}
-
-/**
- * executor - executes commands
- * @tokens: array of tokens
- * @av: double pointer to
- *
- * Return: 0 on success
- */
-int executor(char **tokens, char **av)
-{
-	char *cmd, *path_to_cmd;
-	__pid_t pid;
-	int status;
-	static char *current_directory;
-
-	if (tokens[0] == NULL)
-		return (0);
-
-	cmd = tokens[0];
-	if (strcmp(cmd, "exit") == 0)
-	{
-		free_tokens(tokens);
-		free(current_directory);
-		exit(0);
-	}
-	else if (strcmp(cmd, "cd") == 0)
-	{
-		printf("%s\n", getcwd(current_directory, 1024));
-	}
-	else if (strcmp(cmd, "env") == 0)
-	{
-		print_env();
-	}
-	else
-	{
-		path_to_cmd = path_finder(cmd);
-		pid = fork();
-		if (pid == 0)
-		{
-			if (execve(path_to_cmd, tokens, NULL) == -1)
-			{
-				fprintf(stderr, "%s: 1: %s: not found\n", av[0], cmd);
-				exit(0);
-			}
-		}
-		else if (pid < 0)
-		{
-			perror("Error");
-			exit(0);
-		}
-		else
-		{
-			wait(&status);
-			if (WIFEXITED(status) && WEXITSTATUS(status) == 127)
-			{
-				fprintf(stderr, "%s: 1: %s: not found\n", av[0], cmd);
-			}
-		}
-		free(path_to_cmd);
-	}
-	return (0);
 }
